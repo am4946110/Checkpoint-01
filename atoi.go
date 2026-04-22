bab10a2f113c6a93d467b159319a9577f0f660f2
Amr Maher Fawzy <MaherAmr871@Gmail.com>
2026-04-22 19:50:34 +0200

    first commit

---
+++ atoi.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	s2 := "1234"
+
+	num1, err := strconv.Atoi(s2)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return
+	} else{
+		fmt.Println("Converted integer:", num1)
+	}
+
+	sl := "-1234"
+	num2, err := strconv.Atoi(sl)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", num2)
+		return
+	}
+
+	s3 := "-567"
+	num3, err := strconv.Atoi(s3)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return
+	} else {
+		fmt.Println("Converted integer:", num3)
+	}
+
+
+}
